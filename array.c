23e4f242fc2627f77d07fef1c23c44ec0cf9586c
leahk92 <164601321+leahk92@users.noreply.github.com>
2024-05-07 10:51:33 +0900

    Add files via upload

---
+++ array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	/* 배열의 초기화 */
+		int array_[4] = { 10,20,30 };
+		for (int i = 0; i < 4; i++) {
+			printf("%d ", array_[i]);
+		}
+		printf("\n");
+		// 0 20 30 0
+
+		int array__[4] = { 0 };
+		for (int i = 0; i < 4; i++) {
+			printf("%d ", array__[i]);
+		}
+		printf("\n");
+		// 0 0 0 0
+
+		int array___[4] = { 10 };
+		for (int i = 0; i < 4; i++) {
+			printf("%d ", array___[i]);
+		}
+		printf("\n");
+		// 10 0 0 0
+	
+}
