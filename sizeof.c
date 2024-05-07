7b60ad4f30dffb304db82cd5795f79f9718f3bf7
leahk92 <164601321+leahk92@users.noreply.github.com>
2024-05-07 11:25:29 +0900

    Add files via upload

---
+++ sizeof.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define SIZE 5
+
+int main(void) {
+	
+	int s[SIZE] = { 10,20,30,40,50 };
+	int i;
+
+	for (i = 0; i < SIZE; i++) {
+		printf("s[%d] = %d\n",i,s[i]);
+	}
+
+	/* sizeof 함수 : 바이트 크기를 알려줌 */
+	int score[] = { 10, 20, 30, 40, 50, 60 };
+	int size = sizeof(score) / sizeof(score[0]);
+	// sizeof(score) : 24
+	// sizeof(score[0]) : 4
+	
+	printf("%d\n", size); // size : 6
+	
+	for (i = 0; i < size; i++) {
+		printf("%d ", score[i]);
+	}
+	return 0;
+}
